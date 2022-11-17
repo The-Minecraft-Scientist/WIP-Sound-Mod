d3ea3fe094c04e9d7a175b1fc7126f6859ea3caa
The-Minecraft-Scientist <charlesliske@gmail.com>
2022-11-16 18:33:03 -0800

    Setup "proper" dropping logic for SoundInstances with more callbacks

--- soundmod/src/main/java/net/randomscientist/soundmod/natives/Natives.java
+++ soundmod/src/main/java/net/randomscientist/soundmod/natives/Natives.java
@@ -1,12 +1,14 @@
 package net.randomscientist.soundmod.natives;
 
 import jdk.incubator.foreign.*;
+import net.minecraft.client.sound.SoundInstance;
 import net.randomscientist.soundmod.SoundMod;
 import net.randomscientist.soundmod.util.ResourceDelegator;
 
 import java.lang.annotation.Native;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.util.Map;
 import java.util.Optional;
 import java.util.HashMap;
@@ -20,17 +22,24 @@ public class Natives {
 			JAVA_FLOAT.withName("volume"),
 			JAVA_FLOAT.withName("pitch")
 	);
+	public static GroupLayout RsJavaCallbacks = MemoryLayout.structLayout(
+			ADDRESS.withName("read"),
+			ADDRESS.withName("seek"),
+			ADDRESS.withName("drop")
+	);
 	private static final CLinker linker = CLinker.systemCLinker();
 	private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
 	private static final HashMap<String,FunctionDescriptor> nativeMethods = new HashMap<String,FunctionDescriptor>(Map.of(
-			"init", FunctionDescriptor.of(ADDRESS, ADDRESS.withName("seek_ptr"),ADDRESS.withName("read_ptr")),
+			"init", FunctionDescriptor.of(ADDRESS, RsJavaCallbacks),
 			"add_streaming", FunctionDescriptor.of(ADDRESS,ADDRESS.withName("sender"),RsSoundInstance),
-			"add_static", FunctionDescriptor.of(ADDRESS,ADDRESS.withName("sender"),RsSoundInstance,ADDRESS.withName("buf_ptr"),JAVA_LONG.withName("buf_size"))
+			"add_static", FunctionDescriptor.of(ADDRESS,ADDRESS.withName("sender"),RsSoundInstance,ADDRESS.withName("buf_ptr"),JAVA_LONG.withName("buf_size")),
+			"tick", FunctionDescriptor.of(ADDRESS,ADDRESS.withName("sender"))
 	));
 	private static final HashMap<String,MethodHandle> natives = new HashMap<String,MethodHandle>();
-	public static final HashMap<String, NativeSymbol> methods = new HashMap<String, NativeSymbol>(Map.of(
-			"readStream", tryGenerateNativeSymbol(ResourceDelegator.class,"readStream",FunctionDescriptor.of(JAVA_INT,JAVA_LONG,ADDRESS,JAVA_LONG)),
-			"seekStream", tryGenerateNativeSymbol(ResourceDelegator.class, "seekStream",FunctionDescriptor.of(JAVA_LONG,JAVA_LONG,JAVA_LONG))
+	public static final HashMap<String, NativeSymbol> callbacks = new HashMap<String, NativeSymbol>(Map.of(
+			"readStream", tryGenerateNativeSymbol(ResourceDelegator.class,"readStream", FunctionDescriptor.of(JAVA_INT,JAVA_LONG,ADDRESS,JAVA_LONG)),
+			"seekStream", tryGenerateNativeSymbol(ResourceDelegator.class, "seekStream", FunctionDescriptor.of(JAVA_LONG,JAVA_LONG,JAVA_LONG)),
+			"dropResource", tryGenerateNativeSymbol(ResourceDelegator.class, "dropResource", FunctionDescriptor.ofVoid(JAVA_LONG))
 	));
 	public static MemoryAddress sender;
 	private static NativeSymbol tryGenerateNativeSymbol(Class c,String name, FunctionDescriptor desc) {
@@ -42,6 +51,37 @@ public class Natives {
 		}
 		return linker.upcallStub(handle, desc, ResourceScope.globalScope());
 
+	}
+	public static MemorySegment createSoundStruct(long id, SoundInstance instance, int size) {
+		MemorySegment struct = MemorySegment.allocateNative(RsSoundInstance.byteSize(), ResourceScope.globalScope());
+		VarHandle uuid = ofName(RsSoundInstance,"uuid");
+		VarHandle sizeh = ofName(RsSoundInstance,"size");
+		VarHandle position = RsSoundInstance.varHandle(MemoryLayout.PathElement.groupElement("position"),MemoryLayout.PathElement.sequenceElement(0,1));
+		VarHandle volume = ofName(RsSoundInstance,"volume");
+		VarHandle pitch = ofName(RsSoundInstance,"pitch");
+
+		uuid.set(struct,id);
+		sizeh.set(struct,size);
+		position.set(struct,0, instance.getX());
+		position.set(struct,1, instance.getY());
+		position.set(struct,2, instance.getZ());
+		volume.set(struct, instance.getVolume());
+		pitch.set(struct, instance.getPitch());
+		return struct;
+	}
+	public static VarHandle ofName(GroupLayout struct, String name) {
+		return struct.varHandle(MemoryLayout.PathElement.groupElement(name));
+	}
+	public static MemorySegment createCallbackStruct() {
+		MemorySegment struct = MemorySegment.allocateNative(RsJavaCallbacks.byteSize(), ResourceScope.globalScope());
+		VarHandle read = ofName(RsJavaCallbacks, "read");
+		VarHandle seek = ofName(RsJavaCallbacks, "seek");
+		VarHandle drop = ofName(RsJavaCallbacks, "drop");
+
+		read.set(struct, callbacks.get("readStream").address());
+		seek.set(struct, callbacks.get("seekStream").address());
+		drop.set(struct, callbacks.get("dropResource").address());
+		return struct;
 	}
 	static {
 		Lib.loadNatives();
@@ -57,7 +97,8 @@ public class Natives {
 				}
 		);
 		try {
-			sender = (MemoryAddress) natives.get("init").invoke(methods.get("seekStream").address(),methods.get("readStream").address());
+			MemorySegment struct = createCallbackStruct();
+			sender = (MemoryAddress) natives.get("init").invoke(struct);
 		} catch (Throwable e) {
 			throw new RuntimeException(e);
 		}
@@ -66,6 +107,6 @@ public class Natives {
 		return natives.get(id);
 	}
 	public static NativeSymbol getMethodSymbol(String id) {
-		return  methods.get(id);
+		return  callbacks.get(id);
 	}
 }
