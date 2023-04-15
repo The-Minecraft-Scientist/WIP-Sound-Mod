6c2c5b61911e9bda76cf2ea202d74ef2f86a42df
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2023-04-14 19:56:08 -0700

    start over :))))))

--- README.md
+++
@@ -1,7 +0,0 @@
-# BST MC
-
-Now with Rust:crab:™
-
-## Current capabilities:
-
-Play Minecraft sounds from Rust with `kira`
--- soundmod/src/main/java/net/randomscientist/soundmod/util/WorldChunkAccessor.java
+++
@@ -1,7 +0,0 @@
-package net.randomscientist.soundmod.util;
-
-import java.util.ArrayList;
-
-public interface WorldChunkAccessor {
-    ArrayList<Integer> getSoundScene();
-}
