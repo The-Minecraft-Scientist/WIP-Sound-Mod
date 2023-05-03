c5fc2bc8e3bb782353f112053a18694a0848ac85
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2023-05-02 23:23:57 -0700

    Add all the native methods we need, ""document"" SourceMixin overrides

--- fabric/src/main/java/net/randomscientist/soundmod/mixins/SoundModNative.java
+++ fabric/src/main/java/net/randomscientist/soundmod/mixins/SoundModNative.java
@@ -1,8 +1,23 @@
 package net.randomscientist.soundmod.mixins;
 
+import net.minecraft.util.math.Vec3d;
+
 public class SoundModNative {
     public static native void say_hi();
     public static native void init(Class providerClass);
-    public static native void get_sound_data(String id);
+    public static native void get_sound_data(String path);
     public static native int new_sound_uuid();
+    public static native void close_uuid(int id);
+    public static native void play_uuid(int id);
+    public static native void pause_uuid(int id);
+    public static native void resume_uuid(int id);
+    public static native void stop_uuid(int id);
+    public static native void is_playing_uuid(int id);
+    public static native void is_stopped_uuid( int id);
+    public static native void set_position_uuid(int id, Vec3d pos);
+    public static native void set_pitch_uuid(int id, float pitch);
+    public static native void set_looping_uuid(int id, boolean looping);
+    public static native void set_relative_uuid(int id, boolean relative);
+
+
 }
--- fabric/src/main/java/net/randomscientist/soundmod/mixins/SourceMixin.java
+++ fabric/src/main/java/net/randomscientist/soundmod/mixins/SourceMixin.java
@@ -27,49 +27,52 @@ public class SourceMixin {
     static @Nullable Source create() {
         return newSource(SoundModNative.new_sound_uuid());
     }
+    // Stop playback, close stream, free buffers, delete sources... etc.
     @Overwrite
     public void close() {}
+    //Start playback (initial)
     @Overwrite
     public void play() {}
+    //temporarily pause playback (without freeing/closing backing resources)
     @Overwrite
     public void pause() {}
-
+    // If paused, resume playback. Else, no-op
     @Overwrite
     public void resume() {}
-
+    // I genuinely do not know the difference between AL_STOPPED and AL_PAUSED, but this sets it to STOPPED
     @Overwrite
     public void stop() {}
-
+    // whether this source is currently playing.
     @Overwrite
     public boolean isPlaying() {return true;}
-
+    //Again, openAL shenanigans
     @Overwrite
     public boolean isStopped() {return false;}
-
+    //Sets source position in world space (set_relative = false) or listener-relative space (set_relative = true)
     @Overwrite
     public void setPosition(Vec3d pos) {}
-
+    //Sets source pitch (TODO: check openAL docs on how this should behave)
     @Overwrite
     public void setPitch(float pitch) {}
-
+    //Whether the sound should loop over its buffer (pretty dang sure this should only work for static sounds)
     @Overwrite
     public void setLooping(boolean looping) {}
-
+    //This is a no-op, we're doing some Shenanigans™
     @Overwrite
     public void disableAttenuation() {}
-
+    //This is a no-op, we're doing some Shenanigans™
     @Overwrite
     public void setAttenuation(float attenuation) {}
-
+    // False: sound position is in global, world space coordinates, True: sound position is in local, listener-relative coordinates.
     @Overwrite
     public void setRelative(boolean relative) {}
-
+    //Need to do some jankery here
     @Overwrite
     public void setBuffer(StaticSound sound) {}
-
+    //More jankery :D
     @Overwrite
     public void setStream(AudioStream stream) {}
-
+    //This can probably be a no-op. Nice to have though.
     @Overwrite
     public void tick() {}
 }
