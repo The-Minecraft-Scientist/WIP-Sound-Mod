fb04370aa293bfe53fb4e9c9a4065a7b9fe0f29f
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2023-05-06 17:12:32 -0700

    forgot one :))

---
+++ fabric/src/main/java/net/randomscientist/soundmod/mixins/SoundLoaderMixin.java
@@ -0,0 +1,21 @@
+package net.randomscientist.soundmod.mixins;
+
+import net.minecraft.client.sound.AudioStream;
+import net.minecraft.client.sound.SoundLoader;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import net.randomscientist.soundmod.SoundMod;
+import net.randomscientist.soundmod.sound.RustOggStream;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(SoundLoader.class)
+public class SoundLoaderMixin {
+    @Overwrite
+    public CompletableFuture<AudioStream> loadStreamed(Identifier id, boolean repeatInstantly) {
+        SoundMod.LOGGER.info("loadStreamed called");
+        return CompletableFuture.supplyAsync(() -> new RustOggStream(id), Util.getMainWorkerExecutor());
+    }
+}
